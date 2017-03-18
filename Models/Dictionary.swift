54ed1fa7cb8f7124a4f41b4acec9cce475af3a34
Yuya Hirayama <y@yura.me>
2017-03-18 17:54:56 +0900

    wip

---
+++ Models/Dictionary.swift
@@ -0,0 +1,22 @@
+//
+//  Dictionary.swift
+//  HoneyMustard
+//
+//  Created by Yuya Hirayama on 2017/03/18.
+//  Copyright © 2017年 Yuya Hirayama. All rights reserved.
+//
+
+import Foundation
+
+extension Dictionary {
+  func get<T>(valueForKey key: Key) throws -> T {
+    guard let value = self[key] as? T else {
+      throw DictionaryExtractionError.castFailed(key: String.init(describing: key), actualValue: self[key])
+    }
+    return value
+  }
+}
+
+enum DictionaryExtractionError: Error {
+  case castFailed(key: String, actualValue: Any?)
+}
---
+++ Models/TweetEntity.swift
@@ -0,0 +1,13 @@
+//
+//  TweetEntity.swift
+//  HoneyMustard
+//
+//  Created by Yuya Hirayama on 2017/03/18.
+//  Copyright © 2017年 Yuya Hirayama. All rights reserved.
+//
+
+import Foundation
+
+struct TweetEntity {
+  
+}
