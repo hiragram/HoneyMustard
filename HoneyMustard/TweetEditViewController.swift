71575c87879c0191f8444989296b5245baeadfc4
Yuya Hirayama <y@yura.me>
2017-03-20 17:13:45 +0900

    Add image

--- HoneyMustard/TweetEditViewController.swift
+++ HoneyMustard/TweetEditViewController.swift
@@ -19,9 +19,64 @@ final class TweetEditViewController: UIViewController, StoryboardInstantiatable 
     }
   }
   @IBOutlet fileprivate weak var submitButton: UIButton!
+  @IBOutlet private weak var photo1: UIImageView!
+  @IBOutlet private weak var photo2: UIImageView!
+  @IBOutlet private weak var photo3: UIImageView!
+  @IBOutlet private weak var photo4: UIImageView!
+  @IBOutlet private weak var attachmentContainerHeight: NSLayoutConstraint!
 
-  private let images = Variable<[UIImage]>.init([])
+  @IBOutlet private weak var photo1Width: NSLayoutConstraint!
+  @IBOutlet private weak var photo2Width: NSLayoutConstraint!
+  @IBOutlet private weak var photo3Width: NSLayoutConstraint!
+  @IBOutlet private weak var photo4Width: NSLayoutConstraint!
 
+  private let bag = DisposeBag.init()
+  private let vm = TweetEditViewModel.init()
+
+  override func viewDidLoad() {
+    super.viewDidLoad()
+
+    vm.images.asObservable().subscribe(onNext: { [weak self] (images) in
+      let image1 = images.first
+      let image2 = images.dropFirst(1).first
+      let image3 = images.dropFirst(2).first
+      let image4 = images.dropFirst(3).first
+
+      guard image1 != nil else {
+        self?.attachmentContainerHeight.constant = 0
+        return
+      }
+      self?.attachmentContainerHeight.constant = 60
+
+      if let image = image1 {
+        self?.photo1.image = image
+        self?.photo1Width.constant = 50
+      } else {
+        self?.photo1Width.constant = 0
+      }
+
+      if let image = image2 {
+        self?.photo2.image = image
+        self?.photo2Width.constant = 50
+      } else {
+        self?.photo2Width.constant = 0
+      }
+
+      if let image = image3 {
+        self?.photo3.image = image
+        self?.photo3Width.constant = 50
+      } else {
+        self?.photo3Width.constant = 0
+      }
+
+      if let image = image4 {
+        self?.photo4.image = image
+        self?.photo4Width.constant = 50
+      } else {
+        self?.photo4Width.constant = 0
+      }
+    }).addDisposableTo(bag)
+  }
 }
 
 // - MARK: Observables
---
+++ HoneyMustard/TweetEditViewModel.swift
@@ -0,0 +1,15 @@
+//
+//  TweetEditViewModel.swift
+//  HoneyMustard
+//
+//  Created by Yuya Hirayama on 2017/03/20.
+//  Copyright © 2017年 Yuya Hirayama. All rights reserved.
+//
+
+import UIKit
+import RxSwift
+import RxCocoa
+
+class TweetEditViewModel {
+  let images = Variable<[UIImage]>.init([])
+}
