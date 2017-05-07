12e4a592134635277ac2303419ffdc45f2803519
Yuya Hirayama <y@yura.me>
2017-05-07 18:08:17 +0900

    Fix color

--- HoneyMustard/DefaultStyle.swift
+++ HoneyMustard/DefaultStyle.swift
@@ -11,7 +11,7 @@ import UIKit
 struct DefaultStyle: StyleDefinition {
   // text color
   let generalText: Typography = Typography.init(font: UIFont.systemFont(ofSize: 14), color: #colorLiteral(red: 0.132778734, green: 0.132778734, blue: 0.132778734, alpha: 1), colorForDarkBackground: UIColor.white)
-  let clickableText: Typography = Typography.init(font: UIFont.systemFont(ofSize: 14), color: #colorLiteral(red: 0.1764705926, green: 0.4980392158, blue: 0.7568627596, alpha: 1))
+  let clickableText: Typography = Typography.init(font: UIFont.systemFont(ofSize: 14), color: #colorLiteral(red: 0.2392156869, green: 0.6745098233, blue: 0.9686274529, alpha: 1))
   let usernameText: Typography = Typography.init(font: UIFont.boldSystemFont(ofSize: 14), color: UIColor.black, colorForDarkBackground: UIColor.white)
   let screennameText: Typography = Typography.init(font: UIFont.systemFont(ofSize: 13), color: UIColor.darkGray, colorForDarkBackground: UIColor.lightGray)
   let dateTimeText: Typography = Typography.init(font: UIFont.systemFont(ofSize: 12), color: UIColor.darkGray)
